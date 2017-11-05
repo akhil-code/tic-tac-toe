870e6e0f8600af9ee4a548be95d65f18caf95bf9
Akhil Guttula <guttula.akhil@gmail.com>
2017-11-05 23:19:32 +0530

    completed

--- init.py
+++ init.py
@@ -32,13 +32,80 @@ def drawLines(img):
     cv2.line(img,(0,2*e/3),(e,2*e/3),(255,0,0),5)
     return img
 
+def getPositionFromXY(x,y):
+    global e
+    x_pos,y_pos = (-1,-1)
+
+    if x>0 and x<e/3:
+        x_pos = 1
+    elif x>e/3 and x<(2*e/3):
+        x_pos = 2
+    elif x>(2*e/3) and x<e:
+        x_pos = 3
+
+    if y>0 and y<e/3:
+        y_pos = 0
+    elif y>e/3 and y<(2*e/3):
+        y_pos = 1
+    elif y>(2*e/3) and y<e:
+        y_pos = 2
+
+    return x_pos+y_pos*3
+
+def checkGame(marked):
+    for x in range(0,7,3):
+        if marked[x]==marked[x+1] and marked[x+1]==marked[x+2] and marked[x] != -1:
+            return True
+    for y in range(3):
+        if marked[y]==marked[y+3] and marked[y+3]==marked[y+6] and marked[y] != -1:
+            return True
+
+    if marked[0]==marked[4] and marked[4]==marked[8] and marked[0] != -1:
+        return True
+    if marked[2]==marked[4] and marked[4]==marked[6] and marked[2] != -1:
+        return True
+    return False
 
 
-e = 300
+# mouse callback function
+def clicked(event,x,y,flags,param):
+    global counter,img,marked
+    if event == cv2.EVENT_LBUTTONUP:
+        img = markPosition(img,getPositionFromXY(x,y),counter%2)
+        marked[getPositionFromXY(x,y)-1] = counter%2
+        cv2.imshow("tic tac toe",img)
+        cv2.waitKey(10)
+        if checkGame(marked):
+            cv2.waitKey(1000)
+            if(counter%2)==0:
+                print 'O won the game'
+            else:
+                print 'X won the game'
+            counter = 0
+            marked = [-1 for i in range(9)]
+            img = cv2.imread('img.jpg')
+            img = drawLines(img)
+            return
+        elif counter>=8:
+            cv2.waitKey(1000)
+            print 'match draw'
+            counter = 0
+            marked = [-1 for i in range(9)]
+            img = cv2.imread('img.jpg')
+            img = drawLines(img)
+            return
+        counter += 1
+
+
 img = cv2.imread('img.jpg')
+e,_,_ = img.shape
+marked = [-1 for i in range(9)]
+counter = 0
 img = drawLines(img)
-for x in range(1,10):
-    img = markPosition(img,x,x%2)
+cv2.namedWindow("tic tac toe")
+cv2.setMouseCallback("tic tac toe",clicked)
 
-cv2.imshow("tic tac toe",img)
-cv2.waitKey(0)
+while True:
+    cv2.imshow("tic tac toe",img)
+    if cv2.waitKey(5) & 0xFF == 27:
+        break
